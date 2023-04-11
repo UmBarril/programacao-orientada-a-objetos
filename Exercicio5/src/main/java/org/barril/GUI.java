18582ff326221737308bf76df9fb4dea63fff036
Joao Victor Lima Barros <joao.barros@labs.dcx>
2023-04-11 09:53:56 -0300

    mais exercicio 5

--- Exercicio5/src/main/java/org/barril/GUI.java
+++ Exercicio5/src/main/java/org/barril/GUI.java
@@ -6,32 +6,61 @@ import java.awt.*;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
-import java.net.URL;
+import java.util.LinkedList;
+import java.util.Queue;
 
-public class GUI extends JFrame {
+public class GUI {
+    private JFrame janela;
+    private Queue<Moosica> moosicas = new LinkedList<>();
+    private boolean tocano = true;
+    private JLabel lblImg = new JLabel();
     public GUI() {
-        setTitle("");
-        setLayout(new GridLayout(2, 2));
-        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        setSize(500,500);
+        janela = new JFrame();
+        janela.setTitle("");
+        janela.setLayout(new GridLayout(2, 1));
+        janela.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        janela.setSize(500,500);
 
-        Button addbtn = new Button("adicionar musica");
-        addbtn.setBackground(Color.RED);
+        JButton addbtn = new JButton("<html><center><b>Adicionar música</b><br>");
+        addbtn.addActionListener(e -> {
+            String nomeDaMusica = JOptionPane.showInputDialog("nome da moosica");
+            String linkDaMusica = JOptionPane.showInputDialog("link da moosica");
+            moosicas.add(new Moosica(nomeDaMusica, linkDaMusica));
+        });
+        addbtn.setBackground(Color.DARK_GRAY);
+        addbtn.setForeground(Color.WHITE);
 
-        Button rmbtn = new Button("remover musica");
-        rmbtn.setBackground(Color.RED);
+        JButton rmbtn = new JButton("remover música do topo");
+        rmbtn.addActionListener(e -> moosicas.poll());
+        rmbtn.setBackground(Color.DARK_GRAY);
+        rmbtn.setForeground(Color.WHITE);
 
-        JLabel lblImg = new JLabel();
+        JButton playbtn = new JButton("tocar musica");
+        playbtn.addActionListener(e -> toggleMoosica());
+        playbtn.setBackground(Color.RED);
+        playbtn.setForeground(Color.WHITE);
+
+        janela.add(rmbtn);
+        janela.add(addbtn);
+        janela.add(lblImg);
+        janela.add(playbtn);
+
+        toggleMoosica();
+    }
+    private void toggleMoosica() {
+        tocano = !tocano;
         try {
-            BufferedImage img = ImageIO.read(new File("ovino_moosica.jpg"));
+            String img_path = tocano ? "ovino_moosica.jpg" : "not_music.jpg";
+
+            BufferedImage img = ImageIO.read(new File(img_path));
             ImageIcon ico = new ImageIcon(img.getScaledInstance(250, 250, Image.SCALE_SMOOTH));
             lblImg.setIcon(ico);
-        } catch (IOException e) {
+        } catch (IOException exp) {
             lblImg.setText("se vc esta vendo isso, deu ruim na imagem.");
+            System.out.println("erro: " + exp);
         }
-
-        add(rmbtn);
-        add(addbtn);
-        add(lblImg);
+    }
+    public void setVisible(boolean b) {
+        janela.setVisible(b);
     }
 }
---
+++ Exercicio5/src/main/java/org/barril/Moosica.java
@@ -0,0 +1,10 @@
+package org.barril;
+
+public class Moosica {
+    public String nome;
+    public String link;
+    public Moosica(String nome, String link) {
+        this.nome = nome;
+        this.link = link;
+    }
+}
